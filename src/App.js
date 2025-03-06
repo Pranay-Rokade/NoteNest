2b0dcada2e9377f7552ccb8273544c1209cd476a
Pranay-Rokade <pranayrokade86@gmail.com>
2025-03-06 22:41:41 +0530

    Created Navbar and Routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Navbar from './components/Navbar';
 function App() {
   return (
-    <div className="App">
+    <>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
       <h1>Nest Note</h1>
-    </div>
+    </>
   );
 }
 
