22a15f6df2a834ce8fd881182856987aef67d0eb
Pranay-Rokade <pranayrokade86@gmail.com>
2025-03-10 23:46:18 +0530

    Added a Modal for editing notes

--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -6,7 +6,7 @@ const AddNote = () => {
     const context = useContext(NoteContext);
     const { addNote } = context;
 
-    const [note, setNote] = useState({ title: "", description: "", tag: "default"});
+    const [note, setNote] = useState({ title: "", description: "", tag: ""});
     const handleClick = (e) => {
         e.preventDefault();
         addNote(note.title, note.description, note.tag);
@@ -24,8 +24,12 @@ const AddNote = () => {
                     <input type="text" className="form-control" id="title" name="title" area-descibedby="emailHelp" onChange={onChange}/>
                 </div>
                 <div className="mb-3">
-                    <label htmlFor="description" className="form-label" onChange={onChange}>Description</label>
+                    <label htmlFor="description" className="form-label">Description</label>
                     <input type="text" className="form-control" id="description" name="description" onChange={onChange}/>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="tag" className="form-label">Tag</label>
+                    <input type="text" className="form-control" id="tag" name="tag" onChange={onChange}/>
                 </div>
                 <button type="submit" className="btn btn-primary" onClick={handleClick}>
                     Add Note
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -17,14 +17,13 @@ const NoteState = (props) => {
       }      
     });
     const json = await response.json();
-    console.log(json);
     setNotes(json);
   }
 
   // Add a note
   const addNote = async (title, description, tag) => {
     // API call
-    const response = await fetch(`${host}/api/notes/addNote`, {
+    const response = await fetch(`${host}/api/notes/addnote`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -48,8 +47,18 @@ const NoteState = (props) => {
   }
 
   // Delete a note
-  const deleteNote = (id) => {
+  const deleteNote = async (id) => {
     // API call
+    const response = await fetch(`${host}/api/notes/deletenote/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        'auth-token': "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjdiZjM4MDliYzk3MTU5OTE4YzI0YmI2In0sImlhdCI6MTc0MDkxNzIwNH0.RjXd9wvhp5EDDXMoUu4iu19FT91mMI0LuAjdjowyYqA"
+      }
+    });
+    const json = await response.json();
+
+    // Logic to delete a note in UI
     const newNotes = notes.filter((note)=>{return note._id!==id});
     setNotes(newNotes);
   }
