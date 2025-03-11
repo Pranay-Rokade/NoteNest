29ac5907cc57638183fa361ec1a9af48fb3d1e40
Pranay-Rokade <pranayrokade86@gmail.com>
2025-03-11 22:27:37 +0530

    Added Login component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import About from './components/About';
 import Navbar from './components/Navbar';
 import NoteState from './context/notes/NoteState';
 import Alert from './components/Alert';
+import Login from './components/Login';
+import Signup from './components/Signup';
+
 function App() {
   return (
     <>
@@ -16,6 +19,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Signup />} />
         </Routes>
         </div>
       </Router>
