1d5613b11f7f779eba330f156f5d450ad2b3c997
Pranay-Rokade <pranayrokade86@gmail.com>
2025-03-11 21:28:01 +0530

    Added frontend validation and fixed some errors

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -45,11 +45,11 @@ const Notes = () => {
                         <form className="my-3">
                             <div className="mb-3">
                                 <label htmlFor="title" className="form-label">Title</label>
-                                <input type="text" className="form-control" id="etitle" name="etitle" value={note.etitle} area-descibedby="emailHelp" onChange={onChange}/>
+                                <input type="text" className="form-control" id="etitle" name="etitle" value={note.etitle} area-descibedby="emailHelp" onChange={onChange} minLength={5} required/>
                             </div>
                             <div className="mb-3">
                                 <label htmlFor="description" className="form-label">Description</label>
-                                <input type="text" className="form-control" id="edescription" name="edescription" value={note.edescription} onChange={onChange}/>
+                                <input type="text" className="form-control" id="edescription" name="edescription" value={note.edescription} onChange={onChange} minLength={5} required/>
                             </div>
                             <div className="mb-3">
                                 <label htmlFor="tag" className="form-label">Tag</label>
@@ -59,16 +59,19 @@ const Notes = () => {
                     </div>
                     <div className="modal-footer">
                         <button ref={refClose} type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        <button onClick={handleClick} type="button" className="btn btn-primary">Update Note</button>
+                        <button disabled={note.etitle.length<5 || note.edescription.length<5} onClick={handleClick} type="button" className="btn btn-primary">Update Note</button>
                     </div>
                     </div>
                 </div>
             </div>
             <div className="row my-3">
-            <h2>Your Notes</h2>
-            {notes.map((note)=>{
-                return <Noteitem key={note._id} updateNote={updateNote} note={note}/>
-            })}
+                <h2>Your Notes</h2>
+                <div className="container mx-1">
+                    {notes.length===0 && 'No notes to display'}
+                </div>
+                {notes.map((note)=>{
+                    return <Noteitem key={note._id} updateNote={updateNote} note={note}/>
+                })}
             </div>
         </>
     )
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -16,6 +16,7 @@ const NoteState = (props) => {
         'auth-token': "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjdiZjM4MDliYzk3MTU5OTE4YzI0YmI2In0sImlhdCI6MTc0MDkxNzIwNH0.RjXd9wvhp5EDDXMoUu4iu19FT91mMI0LuAjdjowyYqA"
       }      
     });
+    
     const json = await response.json();
     setNotes(json);
   }
@@ -31,18 +32,9 @@ const NoteState = (props) => {
       },
       body: JSON.stringify({title, description, tag})
     });
-    const json = await response.json();
 
     // Logic to add a note in UI
-    const note = {
-        "_id": "614b4f3d2f0d790c1c6e7c5b",
-        "user": "614b4f3d2f0d7e0c1c6e7c5a",
-        "title": title,
-        "description": description,
-        "tag": tag,
-        "date": "2021-09-22T19:30:21.000Z",
-        "__v": 0
-      };
+    const note = await response.json();
     setNotes(notes.concat(note))
   }
 
@@ -56,8 +48,8 @@ const NoteState = (props) => {
         'auth-token': "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjdiZjM4MDliYzk3MTU5OTE4YzI0YmI2In0sImlhdCI6MTc0MDkxNzIwNH0.RjXd9wvhp5EDDXMoUu4iu19FT91mMI0LuAjdjowyYqA"
       }
     });
-    const json = await response.json();
 
+    const json = await response.json();
     // Logic to delete a note in UI
     const newNotes = notes.filter((note)=>{return note._id!==id});
     setNotes(newNotes);
@@ -74,8 +66,8 @@ const NoteState = (props) => {
       },
       body: JSON.stringify({title, description, tag})
     });
-    const json = await response.json();
 
+    const json = await response.json();
     let newNotes = JSON.parse(JSON.stringify(notes));
     // Logic to edit a note in UI
     for (let index = 0; index < newNotes.length; index++) {
